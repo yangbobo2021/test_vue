1577e1f34d14eb29da3661493a675da6894cdf77
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-12-09 17:11:26 +0800

    Create some.vue

---
+++ some.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+// 更新状态
+const handleUpdateStatus = (id: string, ops: string) => {
+    updateTaskStatusAPI({
+        id,
+        ops: ops === 'ENABLED' ? 'disable' : 'enable'
+    }).then(res => {
+        if (res.status === 2000) {
+            something()
+        }
+    })
+}
+
+const rules = reactive({
+    task_img: [
+        {
+            required: true,
+            // 自定义规则
+            validator: (rule: any, value: any, callback: any) => {
+                if (upload.value._imageUrl) {
+                    callback()
+                } else {
+                    callback(new Error('请上传任务图片'))
+                }
+            }
+        },
+    ]
+}
+</script>
